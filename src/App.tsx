acae73c123f9be531ac5c2e7e2f0c848c0b24caa
minjunine <rnalswn253@naver.com>
2024-08-23 12:46:32 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,24 @@ import './App.css';
 // import CurlyBraces from './component/CurlyBraces';
 import Properties from './component_manage/Properties';
 import Example2 from './component_manage/Example/Example2';
-import { ListFormat } from 'typescript';
+import ListFormat  from 'typescript';
 import ListRender from './component_manage/Example/ListRender';
 
 function App() {
   return (
     <>
       {/* <Component /> */}
-      {/* <FunctionComponent /> */}
+      {/* <FunctionComponent /> */} 
       {/* <CurlyBraces /> */}
       {/* <Properties /> */}
       {/* <ConditionalRender />*/}
-      <Example2 />
+      {/* <Example2 /> */}
       {/* <ListRender /> */}
+      {/* <EventComponent /> */}
+      {/* <StateComponent /> */}
+      {/* <ForwordingStateComponent /> */}
     </>
-  )
-};
+  );
+}
 
 export default App;
